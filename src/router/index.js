268f717d5da241c5bd1a21144609d340d8edeeb8
Marcel Witoschek <marcel.witoschek@gdata.de>
2024-07-14 13:30:26 +0200

    restructure imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Index from "@/pages/Index.vue";
+import { Index } from "@/pages";
 
 export const router = createRouter({
   history: createWebHistory(),
