b296040f44a389c46e91631a80d6a263be66a546
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2024-07-24 17:05:28 +0200

    Mvp frontend (#2)

---
+++ src/components/schema.js
@@ -0,0 +1,45 @@
+import * as yup from "yup";
+
+const validSchema = yup.object().shape({
+  key: yup
+    .string()
+    .required("Autorisierung notwendig!")
+    .test("jwt", "Kein gültiger Schlüssel!", (value) => {
+      if (value === undefined || value === null || value === "") return true;
+      try {
+        const base64Url = value.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(
+          atob(base64)
+            .split("")
+            .map((c) => {
+              return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+        );
+        return JSON.parse(jsonPayload);
+      } catch (e) {
+        return false;
+      }
+    }),
+  ...Array.from({ length: 2 }, (_, i) => ({
+    [`mk-field${i + 1}`]: yup
+      .string()
+      .test("is name valid", "Kein gültiger Name!", (value) => {
+        return value === undefined || value === null || value === ""
+          ? true
+          : /^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$/.test(value);
+      }),
+  })).reduce((acc, curr) => ({ ...acc, ...curr }), {}),
+  ...Array.from({ length: 18 }, (_, i) => ({
+    [`field${i + 1}`]: yup
+      .string()
+      .test("is name valid", "Kein gültiger Name!", (value) => {
+        return value === undefined || value === null || value === ""
+          ? true
+          : /^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$/.test(value);
+      }),
+  })).reduce((acc, curr) => ({ ...acc, ...curr }), {}),
+});
+
+export default validSchema;
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,100 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { Form, Field, ErrorMessage } from "vee-validate";
+
+import validSchema from "@/components/schema";
+
+const isSubmitting = ref(false);
+
+const submit = (entries) => {
+  isSubmitting.value = true;
+  console.log(entries);
+  isSubmitting.value = false;
+};
+</script>
 <template>
-  <div>Hier entsteht etwas grandioses!</div>
+  <Form
+    class="form-container"
+    :validation-schema="validSchema"
+    @submit="submit"
+  >
+    <Field
+      name="key"
+      as="input"
+      type="text"
+      class="field-input"
+      style="width: 200px"
+      placeholder="Schlüssel zur Autorisierung"
+    />
+    <ErrorMessage name="key" style="margin-bottom: 25px" />
+    <div class="field-container">
+      <div v-for="i in 2" :key="i" class="field">
+        <Field
+          :name="`mk-field${i}`"
+          as="input"
+          type="text"
+          class="field-input"
+          :placeholder="`Name des MK${i}`"
+        />
+        <ErrorMessage :name="`mk-field${i}`" />
+      </div>
+    </div>
+    <div class="field-container">
+      <div v-for="i in 18" :key="i" class="field">
+        <Field
+          :name="`field${i}`"
+          as="input"
+          type="text"
+          class="field-input"
+          placeholder="Name des Spielers"
+        />
+        <ErrorMessage :name="`field${i}`" />
+      </div>
+    </div>
+    <button
+      class="btn bg-dark kickerliga-link"
+      type="submit"
+      :disabled="isSubmitting"
+      style="margin-top: 25px"
+    >
+      Abschicken
+      <span
+        v-show="isSubmitting"
+        class="spinner-border spinner-border-sm mr-1"
+      ></span>
+    </button>
+  </Form>
 </template>
-<style scoped></style>
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - var(--header-height) - var(--footer-height));
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.field-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 1329px;
+}
+.field {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.field-input {
+  margin: 5px;
+}
+.field-input::placeholder {
+  text-align: center;
+}
+@media (min-width: 601px) {
+  .field {
+    padding: 10px;
+  }
+}
+</style>
