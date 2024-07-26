aa7eb565b847102f850f4a33ebfb661b7dd5547a
Marcel Witoschek <92092148+Marciland@users.noreply.github.com>
2024-07-27 01:39:54 +0200

    include login, create key and dependency on auth (#5)

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,26 @@
 from argparse import ArgumentParser, Namespace
 
 import uvicorn
-from fastapi import FastAPI, status
+from fastapi import Depends, FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from auth import Auth
 from models import TeamModel
 
 
 def get_arguments() -> Namespace:
-    parser = ArgumentParser(description='Generic API')
+    parser = ArgumentParser(description='Kickerliga-Bochum API')
     parser.add_argument('-d', '--develop', action='store_true', default=False,
                         dest='develop', required=False,
                         help='Local development')
+    parser.add_argument('-p', '--password', dest='password', required=True,
+                        help='Password')
     return parser.parse_args()
 
 
 def start() -> FastAPI | None:
     args = get_arguments()
     docs_url = '/docs' if args.develop else None
+    secret: str = args.password
 
     api = FastAPI(root_path='/kickerliga-bochum/api',
                   docs_url=docs_url, redoc_url=None)
@@ -29,9 +33,19 @@ def start() -> FastAPI | None:
         allow_headers=['*']
     )
 
-    @api.post('/team/create', status_code=status.HTTP_201_CREATED)
+    @api.post('/team/create', status_code=status.HTTP_201_CREATED,
+              dependencies=[Depends(Auth(secret))])
     def create_team(team: TeamModel):
         print(team)
+
+    @api.get('/key', status_code=status.HTTP_200_OK,
+             dependencies=[Depends(Auth(secret).login)])
+    def get_key(team: str) -> str:
+        payload = {
+            'team': team
+            # add exp if wanted
+        }
+        return Auth(secret).create_key(payload)
 
     return api
 
