b2eaedc545ce7a70fa97c7a60f11ca5666fa285d
Marcel Witoschek <marcel.marciland@gmail.com>
2024-07-30 00:06:05 +0200

    validate the model

--- backend/models/team.py
+++ backend/models/team.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from pydantic import BaseModel
 
 
@@ -6,6 +7,20 @@ class TeamModel(BaseModel):
     mk1: str
     mk2: str
     players: list[str]
+
+    def check_validity(self) -> None:
+        if self.name is None:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail='Das sollte nicht passieren!')
+        players = self.get_full_team()
+        if len(players) < 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='Zu wenig Spieler!')
+        for name in players:
+            # replace '-' if it should be allowed
+            if not name.replace(' ', '').isalpha():
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                    detail='Ungültiger Name!')
 
     def get_full_team(self) -> list[str]:
         return [self.mk1, self.mk2] + self.players
