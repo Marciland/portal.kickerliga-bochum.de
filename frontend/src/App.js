9f7780f3c85e2ba4515b7c24bf684f60d1ad3433
Marcel Witoschek <marcel.marciland@gmail.com>
2024-08-12 17:36:55 +0200

    test app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 const resizeMain = () => {
   const header = document.querySelector("header");
-  const footer = document.querySelector("footer");
 
-  if (header && footer) {
+  if (header) {
     document.documentElement.style.setProperty(
       "--header-height",
       `${header.offsetHeight}px`
     );
+  }
+
+  const footer = document.querySelector("footer");
+  if (footer) {
     document.documentElement.style.setProperty(
       "--footer-height",
       `${footer.offsetHeight}px`
