bc3e9b4d6a6a4c9eb696dd5d6f9bcc31ec3132e1
Marcel Witoschek <marcel.marciland@gmail.com>
2024-08-12 04:58:01 +0200

    complete backend unit tests and adapt to kickerliga email

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -1,4 +1,5 @@
 from .auth import Auth
 # switch the origin of dump to write different format
 from .csv import dump_team_to_file
+from .mail import Mail
 from .mail import Smtp as MailClient
--- backend/tests/modules/test_mail.py
+++ backend/tests/modules/test_mail.py
@@ -3,9 +3,9 @@ from email.parser import BytesParser
 from email import policy
 import socket
 import ssl
-
+from unittest.mock import patch, mock_open
 from models import EmailCreds
-from modules import MailClient
+from modules import MailClient, Mail
 
 test_credentials = EmailCreds(email='test@test.test',
                               password='verystrongtestpassword')
@@ -21,8 +21,8 @@ def test_Smtp_init():
 
 def test_Smtp_create_email():
     smtp = MailClient(test_credentials)
-    subject = "Test Subject"
-    content = "Test Content"
+    subject = 'Test Subject'
+    content = 'Test Content'
     mail = smtp.create_email(content=content, subject=subject)
     parser = BytesParser(policy=policy.default)
     parsed_mail = parser.parsebytes(mail.as_bytes())
@@ -33,3 +33,33 @@ def test_Smtp_create_email():
         None
     )
     assert html_part == content
+
+
+def test_Mail_init():
+    subject = 'Test Subject'
+    content = 'Test Content'
+    mail = Mail(content=content, subject=subject)
+    parser = BytesParser(policy=policy.default)
+    parsed_mail = parser.parsebytes(mail.as_bytes())
+    html_part = next(
+        (part.get_payload(decode=True).decode(part.get_content_charset())
+         for part in parsed_mail.iter_parts()
+         if part.get_content_type() == 'text/html'),
+        None
+    )
+    assert html_part == content
+
+
+def test_Mail_attach_file():
+    subject = 'Test Subject'
+    content = 'Test Content'
+    file_name = 'Test file name'
+    file_path = 'Test file path'
+    file_data = b'Test data'
+    mail = Mail(content=content, subject=subject)
+    with patch('builtins.open', mock_open(read_data=file_data)):
+        mail.attach_file(file_path=file_path,
+                         file_name=file_name)
+    attached_part = mail.get_payload()[1]
+    assert attached_part.get_payload(decode=True) == file_data
+    assert attached_part.get_filename() == file_name
